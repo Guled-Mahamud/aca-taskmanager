a9ce3e3c2ffcd9dbfc39a8751fd75895218cab0f
Guled Mahamud <Guleddm92@hotmail.co.uk>
2025-06-08 22:37:26 +0100

    variables.tf update version

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -153,27 +153,10 @@ variable "sku_name" {
   default     = "Standard_AzureFrontDoor"
 }
 
-
-variable "subscription_id" {
-  description = "Azure Subscription ID"
-  type        = string
-}
-
-variable "client_id" {
-  description = "Azure Client ID (Service Principal)"
-  type        = string
-}
-
-variable "tenant_id" {
-  description = "Azure Tenant ID"
-  type        = string
-}
-
-variable "client_secret" {
-  description = "Azure Client Secret"
-  type        = string
-}
-
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
 
 
 # Trigger CI workflow -- -
