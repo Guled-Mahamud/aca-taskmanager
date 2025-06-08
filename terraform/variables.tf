e9d9525c53687b22a6120b917c869dd2fdab0d9e
Guled Mahamud <Guleddm92@hotmail.co.uk>
2025-06-08 18:43:57 +0100

    Upldating the variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -154,4 +154,21 @@ variable "sku_name" {
 }
 
 
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "client_id" {
+  description = "Azure Client ID (Service Principal)"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
+
+
+
 # Trigger CI workflow -
