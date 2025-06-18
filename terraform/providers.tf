22b8749998f6169b0b485bbe43d2550751b19cb0
Guled Mahamud <Guleddm92@hotmail.co.uk>
2025-06-18 22:31:49 +0100

    added changes to terraform plan

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -9,6 +9,8 @@ terraform {
 
 provider "azurerm" {
   features {}
-  use_cli         = true
-  subscription_id = "3f48a398-9208-4440-a19d-dad6871f363c"
+  client_id       = env("ARM_CLIENT_ID")
+  client_secret   = env("ARM_CLIENT_SECRET")
+  subscription_id = env("ARM_SUBSCRIPTION_ID")
+  tenant_id       = env("ARM_TENANT_ID")
 }
