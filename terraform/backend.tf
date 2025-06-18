ecbdbd6bfca2d4ae3447ecc90ba0e565b39b0506
Guled Mahamud <Guleddm92@hotmail.co.uk>
2025-06-18 22:34:06 +0100

    added new information to backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,5 +1,9 @@
 terraform {
   backend "azurerm" {
+    resource_group_name  = "taskmanager-rg"
+    storage_account_name = "taskmanagertfstate"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
 
   }
 }
