fb387b8e31e8814eeae4cff415f4315b1658e126
Guled Mahamud <Guleddm92@hotmail.co.uk>
2025-06-19 00:04:21 +0100

    changes

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,3 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "taskmanager-rg"
-    storage_account_name = "taskmanagertfstate"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-    
-  }
+  backend "azurerm" {}
 }
